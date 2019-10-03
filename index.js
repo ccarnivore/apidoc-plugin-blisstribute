c8e35eb38fd7a0af44cff15ec7615757f89420d5
roman <roman.robel@exitb.de>
2019-10-03 18:19:41 +0200

    copy template to configured schema path

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+let fs     = require('fs-extra');
 let app = {};
 
 const parse = (content, src) => {
+  let destinationPath = app.options.dest + content;
+  let schemaFile = app.options.jsonSchemaPath + content;
+  fs.copySync(schemaFile, destinationPath);
+
   return { schema: content }
 };
 
 module.exports = {
   init(_app) {
     app = _app;
-    app.log.debug('validate' + JSON.stringify(app.options));
+
     app.parsers.apivalidationschema = {
       parse,
       path: 'local',
