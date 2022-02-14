de6ef623a736736c1d08b92ce04d32dd91cf7742
roman <roman.robel@exitb.de>
2022-02-14 13:20:55 +0100

    foo

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const parse = (content, src) => {
   const destinationPath = app.options.dest + content;
   const schemaFile = app.options.jsonSchemaPath + content;
 
-  fs.copyFileSync(schemaFile, destinationPath).then(() => {
-    console.log(content + ' successfully copied')
-  });
+  fs.copyFileSync(schemaFile, destinationPath);
+  console.log(content + ' successfully copied');
 
   return { schema: content }
 };
 
 module.exports = {
-  init(_app) {
+  init: function(_app) {
     app = _app;
 
-    app.parsers.apivalidationschema = {
-      parse,
-      path: 'local',
-      method: 'insert',
+    app.parsers.apischemavalidation = {
+      parse     : parse,
+      path      : 'global',
+      method    : 'insert'
     };
-  },
+  }
 };
